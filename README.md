5333f56149069b5327b4a54449ac527f559d2c5b
haripriya <rm.haripriya@gmail.com>
2023-04-11 19:21:24 +0530

    try

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # c-ref
 
 
+https://www.faceprep.in/c/c-programming-questions/
 
+Simple 
+https://www.w3resource.com/c-programming-exercises/array/index.php
+Array:
+
+Secxond Largest
+DLeft Diagonal
+
+Medium :
+
+https://www.sanfoundry.com/c-program-tic-tac-toe-game/
+
+
+Complex:
+https://www.geeksforgeeks.org/coin-change-dp-7/
--- src/stack.c
+++ src/stack.c
@@ -1,102 +1,50 @@
-// Stack implementation in C
-
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX 10
-
-int count = 0;
-
-// Creating a stack
-struct stack
+void main()
 {
-    int items[MAX];
-    int top;
-};
-typedef struct stack st;
+    int arr1[50], n, i, j = 0, lrg, lrg2nd;
 
-void createEmptyStack(st *s)
-{
-    s->top = -1;
-}
+    printf("\n\nFind the second largest element in an array :\n");
+    printf("-------------------------------------------------\n");
 
-// Check if the stack is full
-int isfull(st *s)
-{
-    if (s->top == MAX - 1)
-        return 1;
-    else
-        return 0;
-}
-
-// Check if the stack is empty
-int isempty(st *s)
-{
-    if (s->top == -1)
-        return 1;
-    else
-        return 0;
-}
-
-// Add elements into stack
-void push(st *s, int newitem)
-{
-    if (isfull(s))
+    printf("Input the size of array : ");
+    scanf("%d", &n);
+    /* Stored values into the array*/
+    printf("Input %d elements in the array :\n", n);
+    for (i = 0; i < n; i++)
     {
-        printf("STACK FULL");
+        printf("element - %d : ", i);
+        scanf("%d", &arr1[i]);
     }
-    else
+    /* find location of the largest element in the array */
+    //   lrg=arr1[0];
+    lrg = 0;
+    for (i = 0; i < n; i++)
     {
-        s->top++;
-        s->items[s->top] = newitem;
+        if (lrg < arr1[i])
+        {
+            lrg = arr1[i];
+            j = i;
+        }
     }
-    count++;
-}
 
-// Remove element from stack
-void pop(st *s)
-{
-    if (isempty(s))
+    /* ignore the largest element and find the 2nd largest element in the array */
+    lrg2nd = 0;
+    for (i = 0; i < n; i++)
     {
-        printf("\n STACK EMPTY \n");
+        if (i == j)
+        {
+            i++; /* ignoring the largest element */
+            i--;
+        }
+        else
+        {
+            if (lrg2nd < arr1[i])
+            {
+                lrg2nd = arr1[i];
+            }
+        }
     }
-    else
-    {
-        printf("Item popped= %d", s->items[s->top]);
-        s->top--;
-    }
-    count--;
-    printf("\n");
-}
 
-// Print elements of stack
-void printStack(st *s)
-{
-    printf("Stack: ");
-    for (int i = 0; i < count; i++)
-    {
-        printf("%d ", s->items[i]);
-    }
-    printf("\n");
-}
-
-// Driver code
-int main()
-{
-    int ch;
-    st *s = (st *)malloc(sizeof(st));
-
-    createEmptyStack(s);
-
-    push(s, 1);
-    push(s, 2);
-    push(s, 3);
-    push(s, 4);
-
-    printStack(s);
-
-    pop(s);
-
-    printf("\nAfter popping out\n");
-    printStack(s);
+    printf("The Second largest element in the array is :  %d \n\n", lrg2nd);
 }
